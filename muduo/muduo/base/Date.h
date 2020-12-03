948392732d0391fae5fcc7144be19e8809e8aada
longxiaotianxiao <770307491@qq.com>
2020-12-03 17:40:59 +0800

    手写Date部分代码

--- muduo/muduo/base/Date.h
+++ muduo/muduo/base/Date.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <string>
 
 namespace muduo
 {
@@ -22,6 +23,38 @@ namespace muduo
 		{}
 		static const int kDaysPerWeek = 7;
 		static const int kJulianDayof1970_01_01;
+
+		explicit Date(const struct tm&);
+
+		void swap(Date& that)
+		{
+			std::swap(julianDayNumber_, that.julianDayNumber_);
+		}
+
+		bool valid() const
+		{
+			return julianDayNumber_ > 0;
+		}
+
+		std::string toIsoString() const;
+
+		struct YearMonthDay yearMonthDay() const;
+
+		int year() const
+		{
+			return yearMonthDay().year;
+		}
+
+		int month() const
+		{
+			return yearMonthDay().month;
+		}
+
+		int day() const
+		{
+			return yearMonthDay().day;
+		}
+
 
 	
 	private:
